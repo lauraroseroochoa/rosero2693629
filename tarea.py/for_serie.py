66de75243b2e9ad9e39e9652817f841861531218
lauraroseroochoa <lauratatianarosero1@gmail.com>
2023-04-28 07:31:15 -0500

    correcion de un enunciado

--- tarea.py/for_serie.py
+++ tarea.py/for_serie.py
@@ -1,6 +1,6 @@
 # Programa que le pida al usuario 
 inicio = int(input('Ingrese un numero inicial para la serie: '))
 fin = int(input('Ingrese numero en el que desea finalizar: '))
-cantidad= int(input('ingrese un numero positivo para incrementar o uno negativo para decrementar: '))
+cantidad= int(input('ingrese un numero positivo para incrementar o para decrementar: '))
 for i in range (inicio,fin,cantidad):
      print (i)
