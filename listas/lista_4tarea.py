caa3e41b302b424df8c29f312e190a05d7254bc4
lauraroseroochoa <lauratatianarosero1@gmail.com>
2023-05-04 19:56:13 -0500

    tarea listas

--- listas/lista_4tarea.py
+++ listas/lista_4tarea.py
@@ -7,14 +7,22 @@ suma=0
 cont=0
 max_lista=0
 min_lista=100000
+moda=0
 print(tam)
+lista_ordenada=lista.sort(lista)
 for i in range(tam):
     num=int(random.randrange(100))
     lista.append(num)
     cont+=1
     suma+=num
+    lista_ordenada=lista.sort(lista)
+#for i in lista:
+ #   if lista[i]==lista[i+1]:
+ #       moda
+print(moda)
     
 promedio=int((suma)/cont)    
 print("La lista es",lista)
+print(lista_ordenada)
 print("La resultado de la suma es: ",suma)
-print("El promedio de los elementos de la listaes: ",promedio)
+print("El promedio o medio de los elementos de la lista es: ",promedio)
