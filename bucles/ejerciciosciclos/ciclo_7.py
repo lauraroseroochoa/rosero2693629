e59ed7da239f1684a6b095199f46f622b88fc899
lauraroseroochoa <lauratatianarosero1@gmail.com>
2023-05-09 08:53:33 -0500

    algunas correciones

--- bucles/ejerciciosciclos/ciclo_7.py
+++ bucles/ejerciciosciclos/ciclo_7.py
@@ -3,10 +3,14 @@ num_positive=1
 mayor=0
 cont=0
 while num_positive>0:
-    num_positive=int (input("ingresa un numero: ")) 
+    num_positive=int (input("ingresa un numero: "))
     cont+=1
+    if num_positive>mayor:
+        mayor=num_positive 
+    else:
+        mayor=mayor
     if num_positive<0:
         print("el numero es negativo")
 cont-=1
 print("la cantidad de numeros positivos ingresados es:",cont)
-print(num_positive)
+print(mayor)
